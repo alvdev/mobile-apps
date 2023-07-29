c215bb4feb481689797da8f42cac2ca2340ad653
Alvaro Devesa <alvdev@outlook.com>
2023-07-29 17:35:04 +0200

    create GradientContainer class

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -2,26 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.blue, Colors.lightGreen],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomRight),
-          ),
-          child: const Center(
-            child: Text(
-              'Demo text',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 48),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.blue, Colors.lightGreen],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          'Demo text',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.white, fontSize: 48),
+        ),
+      ),
+    );
+  }
 }
