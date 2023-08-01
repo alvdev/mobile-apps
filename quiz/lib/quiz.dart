f00cde84ce511d6034b133ee226531cda0cdd565
Alvaro Devesa <alvdev@outlook.com>
2023-08-01 10:41:46 +0200

    refactor: move questions widget to quiz widget

--- quiz/lib/quiz.dart
+++ quiz/lib/quiz.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
-import 'package:quiz/questions_screen.dart';
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
 
-void main() {
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: QuestionsScreen(),
+  @override
+  State<QuestionsScreen> createState() => _QuestionsScreenState();
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  Widget activeScreen = const QuestionsScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color.fromRGBO(83, 1, 152, 1),
       ),
-    ),
-  );
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'demo text',
+              style: TextStyle(
+                  fontSize: 30, color: Color.fromRGBO(255, 255, 255, .6)),
+            ),
+            OutlinedButton(onPressed: () {}, child: const Text('Start Quiz')),
+          ],
+        ),
+      ),
+    );
+  }
 }
