72bf9ab6a4794c7553da3be92a0faf9fb5d41c6e
Alvaro Devesa <alvdev@outlook.com>
2023-08-07 15:31:16 +0200

    add summary data map to questions screen

--- quiz/lib/results_screen.dart
+++ quiz/lib/results_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz/data/questions.dart';
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenQuestions});
 
   final List<String> chosenQuestions;
+
+  List<Map<String, Object>> getSummary() {
+    List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenQuestions.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': chosenQuestions[i],
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenQuestions[i],
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
