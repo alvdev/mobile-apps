54249338eee32c7b18d5896baf76c2b90f8a6e63
Alvaro Devesa <alvdev@outlook.com>
2023-08-30 04:28:05 +0200

    add 'new expenses' widget to expenses overlay

--- expenses/lib/expenses.dart
+++ expenses/lib/expenses.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:expenses/widgets/expenses_list/expenses_list.dart';
 import 'package:expenses/models/expense.dart';
+import 'package:expenses/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -26,9 +27,25 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expenses tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
       body: Column(
         children: [
           const Text('Text 1'),
