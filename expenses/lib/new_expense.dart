298e46295cbb8cca84581a39ca9fc86f8416a504
Alvaro Devesa <alvdev@outlook.com>
2023-08-30 05:56:28 +0200

    feat: add date picker to overlay

--- expenses/lib/new_expense.dart
+++ expenses/lib/new_expense.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// import date formatter
+import 'package:expenses/models/expense.dart';
+
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
@@ -10,6 +13,23 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _dayPicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -48,9 +68,13 @@ class _NewExpenseState extends State<NewExpense> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    const Text('Selected Date'),
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
                     IconButton(
-                      onPressed: () {},
+                      onPressed: _dayPicker,
                       icon: const Icon(Icons.calendar_month),
                     ),
                   ],
