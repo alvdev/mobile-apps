b12b29b071553095c848d2d2de22c8061399f91b
Alvaro Devesa <alvdev@outlook.com>
2023-09-01 03:13:41 +0200

    fix: close overlay on save expense

--- expenses/lib/new_expense.dart
+++ expenses/lib/new_expense.dart
@@ -66,6 +66,7 @@ class _NewExpenseState extends State<NewExpense> {
           date: _selectedDate!,
           category: _selectedCategory),
     );
+    Navigator.pop(context);
   }
 
   @override
