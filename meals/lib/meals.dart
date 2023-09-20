a3b7068cb26cd8b32de36a8819e0cdf29b8319b4
Alvaro Devesa <alvdev@outlook.com>
2023-09-20 23:10:40 +0200

    feat: add dummy grid to home screen

--- meals/lib/meals.dart
+++ meals/lib/meals.dart
@@ -6,8 +6,65 @@ class Meals extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Meals home page')),
-      body: const Text('Meals'),
+      appBar: AppBar(
+        title: const Text('Meals home page'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(20),
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            color: Colors.teal[900],
+            child: const Text('1'),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            color: Colors.red[900],
+            child: const Text('2'),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            color: Colors.yellow[900],
+            child: const Text('3'),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            color: Colors.purple[900],
+            child: const Text('4'),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            color: Colors.indigo[900],
+            child: const Text('5'),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            color: Colors.lime[900],
+            child: const Text('6'),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            color: Colors.green[900],
+            child: const Text('7'),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            color: Colors.blue[900],
+            child: const Text('8'),
+          ),
+        ],
+      ),
     );
   }
 }
