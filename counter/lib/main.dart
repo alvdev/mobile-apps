4926ae94560250d71c696d417221d007c0c4a702
Alvaro Devesa <alvdev@outlook.com>
2023-09-22 21:19:16 +0200

    feat: add counter screen

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:counter/presentation/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello world!'),
-        ),
-      ),
+      home: CounterScreen(),
     );
   }
 }
