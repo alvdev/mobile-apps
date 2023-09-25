ed0aae4ed2f7394f599ef177614821ebea689550
Alvaro Devesa <alvdev@outlook.com>
2023-09-25 03:37:46 +0200

    feat: add chat screen with app bar

--- yes_no/lib/main.dart
+++ yes_no/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:yes_no/config/theme/app_theme.dart';
+import 'package:yes_no/presentation/screens/chat_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,14 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme(globalColor: 5).theme(),
-      home: Scaffold(
+      home: const ChatScreen(),
+      /* Scaffold(
         body: Center(
           child: FilledButton(
             child: const Text('Click me!'),
             onPressed: () {},
           ),
         ),
-      ),
+      ), */
     );
   }
 }
---
+++ yes_no/lib/presentation/screens/chat_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const CircleAvatar(
+          backgroundImage: NetworkImage(
+              'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+        ),
+        title: const Text('Chat Screen'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
