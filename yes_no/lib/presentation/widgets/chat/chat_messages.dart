5b10e1d6acdb6651353b76f8713750cf91b42472
Alvaro Devesa <alvdev@outlook.com>
2023-09-26 16:21:44 +0200

    feat: add input message field

--- yes_no/lib/presentation/widgets/chat/chat_messages.dart
+++ yes_no/lib/presentation/widgets/chat/chat_messages.dart
@@ -19,8 +19,42 @@ class ChatMessages extends StatelessWidget {
                   : const MyBubbleMessage(),
             ),
           ),
-          const Text('Chat Screen'),
+          MessageField(),
         ],
+      ),
+    );
+  }
+}
+
+class MessageField extends StatelessWidget {
+  const MessageField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final inputTextController = TextEditingController();
+    final inputFocus = FocusNode();
+
+    return TextFormField(
+      controller: inputTextController,
+      focusNode: inputFocus,
+      onTapOutside: (event) => inputFocus.unfocus(),
+      onFieldSubmitted: (value) {
+        print('Valor: ${inputTextController.value.text}');
+        inputTextController.clear();
+        inputFocus.requestFocus();
+      },
+      decoration: InputDecoration(
+        hintText: 'Enter your message',
+        contentPadding: const EdgeInsets.all(20),
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            final text = inputTextController.value.text;
+            print(text);
+            inputTextController.clear();
+          },
+        ),
       ),
     );
   }
