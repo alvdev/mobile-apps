9325b1a5397d9383af683cb427aa58df0ccd20fc
Alvaro Devesa <alvdev@outlook.com>
2023-09-27 06:41:30 +0200

    add chat provider

---
+++ yes_no/lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:yes_no/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messages = [
+    Message(message: 'Hello', fromWho: FromWho.me),
+    Message(message: 'Hi', fromWho: FromWho.him),
+  ];
+}
