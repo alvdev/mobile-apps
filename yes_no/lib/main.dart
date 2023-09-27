dcf89201c84a7ea9d1bfb882784a432efa90a1bf
Alvaro Devesa <alvdev@outlook.com>
2023-09-27 06:57:46 +0200

    add Provider globally

--- yes_no/lib/main.dart
+++ yes_no/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:yes_no/config/theme/app_theme.dart';
 import 'package:yes_no/presentation/screens/chat_screen.dart';
+import 'package:yes_no/presentation/providers/chat_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,18 +14,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(globalColor: 5).theme(),
-      home: const ChatScreen(),
-      /* Scaffold(
-        body: Center(
-          child: FilledButton(
-            child: const Text('Click me!'),
-            onPressed: () {},
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ChatProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(globalColor: 5).theme(),
+        home: const ChatScreen(),
+        /* Scaffold(
+          body: Center(
+            child: FilledButton(
+              child: const Text('Click me!'),
+              onPressed: () {},
+            ),
           ),
-        ),
-      ), */
+        ), */
+      ),
     );
   }
 }
--- yes_no/lib/presentation/providers/chat_provider.dart
+++ yes_no/lib/presentation/providers/chat_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:yes_no/domain/entities/message.dart';
+import 'package:yes_no/domain/entities/message_entity.dart';
 
 class ChatProvider extends ChangeNotifier {
   List<Message> messages = [
