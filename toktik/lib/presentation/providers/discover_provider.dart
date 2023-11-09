c1ab29c4f0120e86f043dffdfa955388e2488493
Alvaro Devesa <alvdev@outlook.com>
2023-11-09 02:23:49 +0100

    feat: add fetching video data from video post model

--- toktik/lib/presentation/providers/discover_provider.dart
+++ toktik/lib/presentation/providers/discover_provider.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/shared/data/local_video_posts.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // TODO: Repository, DataSource
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
     // TODO: Load next page
+    await Future.delayed(const Duration(seconds: 2));
 
+    final List<VideoPost> fetchedVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
+
+    videos.addAll(fetchedVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
