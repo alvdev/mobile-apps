1b12d1481129dccc5cf802327fae16d15495152d
Alvaro Devesa <alvdev@outlook.com>
2024-02-26 20:16:34 +0100

    ✨ feat: add AppTheme config class

---
+++ widgets/lib/config/theme/theme_data.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const List colors = <Color>[
+  Colors.red,
+  Colors.purple,
+  Colors.blue,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor < colors.length,
+            'Selected color must be less than ${colors.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+        brightness: Brightness.dark,
+        colorSchemeSeed: colors[selectedColor],
+      );
+}
