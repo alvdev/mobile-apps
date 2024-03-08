d0d8f66bbe472c47c6316084c454a23a2c31e930
Alvaro Devesa <alvdev@outlook.com>
2024-03-08 16:00:09 +0100

    add home screen

--- widgets/lib/main.dart
+++ widgets/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets/config/theme/theme_data.dart';
+import 'package:widgets/presentation/screeens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,19 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Widgets App',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme(selectedColor: 1).getTheme(),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Widgets App'),
-            backgroundColor: Colors.teal,
-            centerTitle: true,
-          ),
-          body: Center(
-            child:
-                FilledButton(onPressed: () {}, child: const Text('Click me!')),
-          ),
-        ));
+      title: 'Widgets App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 1).getTheme(),
+      home: const HomeScreen(),
+    );
   }
 }
