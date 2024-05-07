808dcf7a233c33eb93c5fe49af0c55e74b3730b7
Alvaro Devesa <alvdev@outlook.com>
2024-05-07 23:09:22 +0200

    add home screen

---
+++ contradefensa/lib/presentation/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const name = 'home-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
