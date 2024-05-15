a0b297c3527fbd4f115e42f165a48b003ecb05c4
Alvaro Devesa <alvdev@outlook.com>
2024-05-15 20:21:03 +0200

    chore: add riverpod

--- contradefensa/lib/main.dart
+++ contradefensa/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:contradefensa/config/theme/app_theme.dart';
 import 'package:contradefensa/config/router/app_router.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
 
-  runApp(const Contra());
+  runApp(const ProviderScope(child: Contra()));
 }
 
 class Contra extends StatelessWidget {
