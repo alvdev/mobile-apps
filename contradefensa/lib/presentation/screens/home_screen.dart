d02ff3fb52f61c50783673d681702a05adf7f552
Alvaro Devesa <alvdev@outlook.com>
2024-05-28 06:43:44 +0200

    (home): add 10 questions

--- contradefensa/lib/presentation/screens/home_screen.dart
+++ contradefensa/lib/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
+import 'package:contradefensa/presentation/providers/questions/questions_providers.dart';
 import 'package:flutter/material.dart';
-import 'package:contradefensa/config/constants/environment.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -8,15 +9,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(Environment.quizApiKey),
-          ],
-        ),
-      ),
+    return const Scaffold(
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ref.read(questionsNumberProvider.notifier).loadNextQuestions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final questions = ref.watch(questionsNumberProvider);
+    return ListView.builder(
+      itemCount: questions.length,
+      itemBuilder: (context, index) {
+        final question = questions[index];
+        return ListTile(title: Text(question.question));
+      },
     );
   }
 }
