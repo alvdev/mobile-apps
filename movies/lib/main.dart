7277d046b96316b010dbaaef7e9507be72951196
Alvaro Devesa <alvdev@outlook.com>
2024-06-11 10:06:12 +0200

    initial commit

---
+++ movies/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Movies app',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('This is the appbar'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('This is the body'),
+      ),
+    );
+  }
+}
