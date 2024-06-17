f6a6c14415514ae73aa0cf57208066f26010889f
Alvaro Devesa <alvdev@outlook.com>
2024-06-17 11:22:36 +0200

    add game entity

---
+++ freegames/lib/domain/entities/game_entity.dart
@@ -0,0 +1,27 @@
+class GameEntity {
+  final int id;
+  final String title;
+  final String thumbnail;
+  final String description;
+  final String url;
+  final String gameUrl;
+  final String genre;
+  final String platform;
+  final String publisher;
+  final String developer;
+  final DateTime releaseDate;
+
+  const GameEntity({
+    required this.id,
+    required this.title,
+    required this.thumbnail,
+    required this.description,
+    required this.url,
+    required this.gameUrl,
+    required this.genre,
+    required this.platform,
+    required this.publisher,
+    required this.developer,
+    required this.releaseDate,
+  });
+}
