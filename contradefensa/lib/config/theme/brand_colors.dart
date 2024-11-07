60e4219f290201ab1eba6dc0f2fb1b82ab272da4
Alvaro Devesa <alvdev@outlook.com>
2024-11-07 22:52:07 +0100

    add initial app colors config

---
+++ contradefensa/lib/config/theme/brand_colors.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  // Whites
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const white300 = Color.fromRGBO(255, 255, 255, 0.3);
+  static const white500 = Color.fromRGBO(255, 255, 255, 0.5);
+  static const white700 = Color.fromRGBO(255, 255, 255, 0.7);
+  static const white900 = Color.fromRGBO(255, 255, 255, 0.9);
+
+  // Blacks
+  static const black = Color.fromRGBO(0, 0, 0, 1);
+  static const black300 = Color.fromRGBO(0, 0, 0, 0.3);
+  static const black500 = Color.fromRGBO(0, 0, 0, 0.5);
+  static const black700 = Color.fromRGBO(0, 0, 0, 0.7);
+  static const black900 = Color.fromRGBO(0, 0, 0, 0.9);
+
+  // Grays
+  static const gray300 = Color.fromRGBO(209, 213, 219, 1);
+  static const gray500 = Color.fromRGBO(107, 114, 128, 1);
+  static const gray700 = Color.fromRGBO(55, 65, 81, 1);
+
+  // Reds
+  static const red300 = Color.fromRGBO(252, 165, 165, 1);
+  static const red500 = Color.fromRGBO(239, 68, 68, 1);
+  static const red700 = Color.fromRGBO(158, 0, 0, .85);
+
+  // Yellows
+  static const yellow300 = Color.fromRGBO(253, 224, 71, 1);
+  static const yellow500 = Color.fromRGBO(234, 179, 8, 1);
+  static const yellow700 = Color.fromRGBO(161, 98, 7, 1);
+
+  // Blues
+  static const blue300 = Color.fromRGBO(147, 197, 253, 1);
+  static const blue500 = Color.fromRGBO(59, 130, 246, 1);
+  static const blue700 = Color.fromRGBO(29, 78, 216, 1);
+
+  // Greens
+  static const green300 = Color.fromRGBO(134, 239, 172, 1);
+  static const green500 = Color.fromRGBO(34, 197, 94, 1);
+  static const green700 = Color.fromRGBO(21, 128, 61, 1);
+
+  // Teals
+  static const teal300 = Color.fromRGBO(94, 234, 212, 1);
+  static const teal500 = Color.fromRGBO(20, 184, 166, 1);
+  static const teal700 = Color.fromRGBO(15, 118, 110, 1);
+}
--- contradefensa/lib/config/theme/theme.dart
+++ contradefensa/lib/config/theme/theme.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:contradefensa/config/theme/brand_colors.dart';
 
 class AppTheme {
   AppTheme._();
 
-  static TextTheme text = TextTheme(
+  static final text = TextTheme(
     displaySmall: TextStyle(fontSize: 12),
     displayMedium: TextStyle(fontSize: 24),
     displayLarge: TextStyle(fontSize: 36),
   );
-
-  static ThemeData dark = ThemeData(
-    brightness: Brightness.dark,
-    fontFamily: 'Consolas',
-    primaryColor: Colors.teal,
+  
+  static const darkColorScheme = ColorScheme.dark(
+    primary: AppColor.white,
+    onPrimary: AppColor.teal700,
+    secondary: AppColor.green700,
+    onSecondary: AppColor.yellow300,
+    surface: AppColor.teal500,
+    onSurface: AppColor.white900,
+    error: AppColor.red700,
+    onError: AppColor.red300,
+    brightness: Brightness.light,
   );
+
+  static final dark = ThemeData(
+    colorScheme: darkColorScheme,
+    canvasColor: darkColorScheme.surface,
+    scaffoldBackgroundColor: darkColorScheme.surface,
+    fontFamily: GoogleFonts.inconsolata(fontWeight: FontWeight.w600).fontFamily,
+    textTheme: AppTheme.text,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColor.red700,
+    ),
+    bottomAppBarTheme: BottomAppBarTheme(
+      color: Color.fromRGBO(255, 142, 49, 0.847),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(),
+  );
+  
 }
