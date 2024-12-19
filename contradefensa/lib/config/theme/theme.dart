9f9463e49e92eae47bec67f10a39f55f16fb4f00
Alvaro Devesa <alvdev@outlook.com>
2024-12-19 16:26:13 +0100

    add custom default theme elevated button

--- contradefensa/lib/config/theme/theme.dart
+++ contradefensa/lib/config/theme/theme.dart
@@ -6,7 +6,7 @@ class AppTheme {
   AppTheme._();
 
   static final text = TextTheme(
-    displaySmall: TextStyle(fontSize: 12),
+    displaySmall: TextStyle(fontSize: 16),
     displayMedium: TextStyle(fontSize: 24),
     displayLarge: TextStyle(fontSize: 36),
   );
@@ -16,7 +16,7 @@ class AppTheme {
     onPrimary: AppColor.teal700,
     secondary: AppColor.green700,
     onSecondary: AppColor.yellow300,
-    surface: AppColor.teal500,
+    surface: AppColor.black700,
     onSurface: AppColor.white900,
     error: AppColor.red700,
     onError: AppColor.red300,
@@ -35,7 +35,27 @@ class AppTheme {
     bottomAppBarTheme: BottomAppBarTheme(
       color: Color.fromRGBO(255, 142, 49, 0.847),
     ),
-    elevatedButtonTheme: ElevatedButtonThemeData(),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        textStyle: WidgetStatePropertyAll(
+          TextStyle(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor:
+            WidgetStatePropertyAll(Colors.cyan.shade400.withOpacity(0.9)),
+        foregroundColor: WidgetStatePropertyAll(Colors.black),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+        ),
+        elevation: WidgetStatePropertyAll(30),
+        padding: WidgetStateProperty.all(
+          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        ),
+      ),
+    ),
   );
   
 }
